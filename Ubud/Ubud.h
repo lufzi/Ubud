709579b700133d1974d1c94d5e5dd4b1c526bdcb
Luqman Fauzi <luckman.fauzi@gmail.com>
2017-10-22 18:36:00 +0800

    Intial commit

---
+++ Ubud/Ubud.h
@@ -0,0 +1,19 @@
+//
+//  Ubud.h
+//  Ubud
+//
+//  Created by Luqman Fauzi on 22/10/2017.
+//  Copyright © 2017 Luqman Fauzi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Ubud.
+FOUNDATION_EXPORT double UbudVersionNumber;
+
+//! Project version string for Ubud.
+FOUNDATION_EXPORT const unsigned char UbudVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Ubud/PublicHeader.h>
+
+
---
+++ UbudTests/UbudTests.swift
@@ -0,0 +1,17 @@
+//
+//  UbudTests.swift
+//  UbudTests
+//
+//  Created by Luqman Fauzi on 22/10/2017.
+//  Copyright © 2017 Luqman Fauzi. All rights reserved.
+//
+
+import XCTest
+@testable import Ubud
+
+final class UbudTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+    }
+}
